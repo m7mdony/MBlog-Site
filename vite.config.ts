e46832bb55e4d27fa8cebf4f756527232ed3b2cc
m7mdony <m7mdony132@gmail.com>
2025-08-17 22:31:04 +0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -10,14 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
     strictPort: true,
-    headers: {
-      'X-Content-Type-Options': 'nosniff',
-      'Content-Type': 'application/javascript; charset=utf-8'
-    },
-    fs: {
-      strict: false,
-      allow: ['..']
-    },
+    
     middlewareMode: false
   },
   build: {
@@ -43,10 +36,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'],
-    force: true
-  },
+
   define: {
     __DEV__: mode === 'development'
   }
